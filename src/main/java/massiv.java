751b5cbbb41b3f952e3bd4d0fde903ef7f188d30
Skalapendra1 <ms.volkova1993@mail.ru>
2021-06-29 00:03:47 +0300

    правка массива

--- src/main/java/massiv.java
+++ src/main/java/massiv.java
@@ -18,17 +18,24 @@ public class massiv {
             if ((mas[i] >= a) && (mas[i] < 0)) {
                 a = mas[i];
                 j = i;
-            } else if ((mas[i] > 0) && (mas[i] <= b)) {
+            }
+            else if ((mas[i] > 0) && (mas[i] <= b)) {
                 b = mas[i];
                 k = i;
             }
         }
         System.out.println("\nМаксимальное отрицательное --- " + a + " индекс " + j + "\nМинимальное положительное --- " + b + " индекс " + k);
-        int tmp = mas[j];
-        mas[j] = mas[k];
-        mas[k] = tmp;
         System.out.println("\nНовый массив");
         for (int i = 0; i < 20; i++) {
+            if (mas[i] == a) {
+                mas[i]=b;
+            }
+            else if (mas[i]==b){
+                mas[i]=a;
+            }
+        }
+
+        for (int i= 0; i < 20; i++) {
             System.out.print(mas[i] + "   ");
         }
     }
